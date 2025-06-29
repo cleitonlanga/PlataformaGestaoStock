df1b61344fcc346a4c054d0f3bfead12680996c8
cleiton langa <cleytonlanga10@gmail.com>
2025-06-29 15:18:03 +0200

    small changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,10 +3,13 @@ import axios from "axios";
 
 const isElectron = window?.process?.versions?.electron;
 
+// Usa variáveis de ambiente para produção
+const baseURL = isElectron
+  ? "http://localhost:5000/api"
+  : "http://localhost:5000/api";
+
 const api = axios.create({
-  baseURL: isElectron
-    ? "http://localhost:5000/api"
-    : "http://localhost:5000/api",
+  baseURL,
 });
 
 export default api;
