f8984685e34784d29573a5a98edd29dc935023ed
cleiton langa <cleytonlanga10@gmail.com>
2025-07-04 02:09:10 +0200

    changed the gateway and .env

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 // Usa variáveis de ambiente para produção
 const baseURL = import.meta.env.VITE_API_URL;
+console.log(`Base URL: ${baseURL}`);
 
 const api = axios.create({
   baseURL,
